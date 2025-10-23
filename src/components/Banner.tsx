e795bbcb157bbe9a0209428c3e830352af6a79d3
varun <varun4socials@gmail.com>
2025-10-23 12:24:54 +0530

    Fill banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,15 @@
 function Banner() {
     return (
-        <div id="hero-banner" className="">
-            Banner
+        <div id="hero-banner">
+            <div className="text-3xl font-bold">
+                <h2>Earn up to 8.15% on</h2>
+                <h2>RBI Regulated FDs</h2>
+            </div>
+            <a href="https://www.superfd.in/fd/cmc8t2dur0015c8w0mfg0trca">
+                <button id="know-more" className="my-4">
+                    Know More
+                </button>
+            </a>
         </div>
     );
 }
