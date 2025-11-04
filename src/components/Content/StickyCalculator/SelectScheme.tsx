487a35fa77c4929a8c19bbedc477f474b3841569
varun <varun4socials@gmail.com>
2025-11-04 23:04:54 +0530

    Replace sticky calculator select dropdown with shadcn component

--- src/components/Content/StickyCalculator/SelectScheme.tsx
+++ src/components/Content/StickyCalculator/SelectScheme.tsx
@@ -1,35 +1,56 @@
 import { Label } from "@/components/ui/label";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 import { suryoday } from "@/constants/db";
 import type { selectSchemeProps } from "@/constants/interfaces";
 
 function SelectScheme({ setScheme, setQuota, quota }: selectSchemeProps) {
-    function handleChangeScheme(e: React.ChangeEvent<HTMLSelectElement>) {
-        setScheme(Number(e.target.value));
-    }
-
     function handleChangeQuota(checked: boolean | "indeterminate") {
         setQuota(Boolean(checked));
+    }
+
+    function handleChangeSelection(arg: string) {
+        setScheme(Number(arg));
     }
 
     return (
         <div>
             <fieldset className="m-4 p-4 border-2 border-gray-700 rounded-lg">
                 <legend className="font-semibold px-2">Scheme</legend>
-                <select
-                    name="sticky-scheme"
-                    id="sticky-scheme"
-                    onChange={handleChangeScheme}
-                >
-                    <option value="" disabled>
-                        Select
-                    </option>
-                    {suryoday.schemes.map((scheme) => (
-                        <option value={scheme.tenure}>{`${
-                            quota ? scheme.rate.senior : scheme.rate.regular
-                        }% (${scheme.tenure}Y)`}</option>
-                    ))}
-                </select>
+                <Select onValueChange={handleChangeSelection}>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select a scheme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            {suryoday.schemes.map((fd) => {
+                                if (fd.tenure !== 0) {
+                                    const interest = quota
+                                        ? fd.rate.senior
+                                        : fd.rate.regular;
+                                    return (
+                                        <SelectItem
+                                            value={String(fd.tenure)}
+                                            key={fd.tenure}
+                                        >
+                                            {interest +
+                                                "% (" +
+                                                fd.tenure +
+                                                "Y)"}
+                                        </SelectItem>
+                                    );
+                                }
+                            })}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
             </fieldset>
             <div className="m-5 flex items-center space-x-2">
                 <Switch id="quota" onCheckedChange={handleChangeQuota} />
